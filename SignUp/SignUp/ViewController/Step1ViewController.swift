5b7b4788e1604d2d6ae0acd14ff5dd33e53eda49
Jihee hwang <hwangjihee94@gmail.com>
2022-03-30 16:21:53 +0900

    [ Add ] placeholder

--- SignUp/SignUp/ViewController/Step1ViewController.swift
+++ SignUp/SignUp/ViewController/Step1ViewController.swift
@@ -15,6 +15,8 @@ class Step1ViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var button: UIButton!
     
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,21 +31,25 @@ class Step1ViewController: UIViewController {
     func setIdField() {
         idField.frame = CGRect(x: 35, y: 250 , width: 320, height: 50)
         idField.borderStyle = .roundedRect
+        idField.placeholder = "영문 소문자, 숫자, 특수기호(_, -), 5-20자"
     }
     
     func setPasswordField() {
         passwordField.frame = CGRect(x: 35, y: 355 , width: 320, height: 50)
         passwordField.borderStyle = .roundedRect
+        passwordField.placeholder = "영문 대/소문자, 숫자, 특수문자(!@#$%), 8-16자"
     }
     
     func setPasswordCheckField() {
         passwordCheckField.frame = CGRect(x: 35, y: 460 , width: 320, height: 50)
         passwordCheckField.borderStyle = .roundedRect
+        passwordCheckField.placeholder = "설정한 비밀번호를 입력해주세요."
     }
     
     func setNameField() {
         nameField.frame = CGRect(x: 35, y: 565 , width: 320, height: 50)
         nameField.borderStyle = .roundedRect
+        nameField.placeholder = "이름을 입력해주세요."
     }
     
     func setButton() {
