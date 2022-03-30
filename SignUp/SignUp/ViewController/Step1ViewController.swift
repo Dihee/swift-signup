38edc1b124d60e875e8289c59ce02d28fee140a3
Jihee hwang <hwangjihee94@gmail.com>
2022-03-30 21:53:58 +0900

    [ Create ] Scroll View, KetBoard motion
    키보드에 가려지는 텍스트필드 스크롤 뷰와, 노티피케이션으로 보이게 개선 및 버튼 누르면 키보드 닫힘 버튼 추가

--- SignUp/SignUp/ViewController/Step1ViewController.swift
+++ SignUp/SignUp/ViewController/Step1ViewController.swift
@@ -15,7 +15,7 @@ class Step1ViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var button: UIButton!
     
- 
+    private var keyHeight: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,29 +25,38 @@ class Step1ViewController: UIViewController {
         setPasswordCheckField()
         setNameField()
         setButton()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    @IBAction func nextButton(_ sender: UIButton) {
+        idField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        passwordCheckField.resignFirstResponder()
+        nameField.resignFirstResponder()
+    }
     
     func setIdField() {
-        idField.frame = CGRect(x: 35, y: 250 , width: 320, height: 50)
+        idField.frame = CGRect(x: 35, y: 207 , width: 320, height: 50)
         idField.borderStyle = .roundedRect
         idField.placeholder = "영문 소문자, 숫자, 특수기호(_, -), 5-20자"
     }
     
     func setPasswordField() {
-        passwordField.frame = CGRect(x: 35, y: 355 , width: 320, height: 50)
+        passwordField.frame = CGRect(x: 35, y: 315 , width: 320, height: 50)
         passwordField.borderStyle = .roundedRect
         passwordField.placeholder = "영문 대/소문자, 숫자, 특수문자(!@#$%), 8-16자"
     }
     
     func setPasswordCheckField() {
-        passwordCheckField.frame = CGRect(x: 35, y: 460 , width: 320, height: 50)
+        passwordCheckField.frame = CGRect(x: 35, y: 423 , width: 320, height: 50)
         passwordCheckField.borderStyle = .roundedRect
         passwordCheckField.placeholder = "설정한 비밀번호를 입력해주세요."
     }
     
     func setNameField() {
-        nameField.frame = CGRect(x: 35, y: 565 , width: 320, height: 50)
+        nameField.frame = CGRect(x: 35, y: 532 , width: 320, height: 50)
         nameField.borderStyle = .roundedRect
         nameField.placeholder = "이름을 입력해주세요."
     }
@@ -55,8 +64,23 @@ class Step1ViewController: UIViewController {
     func setButton() {
         button.tintColor = UIColor(red: 235 / 255, green: 155 / 255, blue: 100 / 255, alpha: 1)
     }
+
+    @objc func keyboardWillShow(_ sender: Notification) {
+        let userInfo: NSDictionary = sender.userInfo! as NSDictionary
+        let keyboardFrame: NSValue = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        let keyboardHeight = keyboardRectangle.height
+        
+        keyHeight = keyboardHeight
+        
+        self.view.frame.size.height -= keyboardHeight
+    }
+
+    @objc func keyboardWillHide(_ sender: Notification) {
+        guard let keyHeight = keyHeight else { return }
+        self.view.frame.size.height += keyHeight
+    }
     
 
 
 }
-
